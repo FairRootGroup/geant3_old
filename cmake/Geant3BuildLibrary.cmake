509403156ab013d65d3df24b0b85df8e10c9fc7f
Florian Uhlig <f.uhlig@gsi.de>
2015-10-21 15:04:35 +0200

    Add gcalor and add it properly to the build system such that it is compiled.

--- cmake/Geant3BuildLibrary.cmake
+++ cmake/Geant3BuildLibrary.cmake
@@ -52,7 +52,7 @@ ROOT_GENERATE_DICTIONARY(
 set(directories
     added gbase gcons geocad ggeom gheisha ghits ghrout ghutils giface giopa
     gkine gparal gphys gscan gstrag gtrak matx55 miface miguti neutron peanut
-    fiface cgpack fluka block comad erdecks erpremc minicern gdraw)
+    fiface cgpack fluka block comad erdecks erpremc minicern gdraw gcalor)
 
 # Fortran sources
 set(fortran_sources gcinit.F)
